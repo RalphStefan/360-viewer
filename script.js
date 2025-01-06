c0b4e714c7fc19a25c7542e4625880c9820a5eb7
RalphStefan <ralphvandervoet@gmail.com>
2025-01-06 22:32:27 +0100

    x3

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
             "type": "equirectangular",
             "panorama": "images/ingang.JPG",
             'etage': 0, 
-            "info-box": "Welkom in scène i1",
+            "infotext": "Welkom in scène i1",
             'mapPosition': { 'x': 86, 'y': 184 },
             "noordPunt": 90, // Noordpunt in graden voor scene 1
             "hotSpots": [
